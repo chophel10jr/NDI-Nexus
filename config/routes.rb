5f4f3a6f0bf82040bf82b1d9b4ed73d0758bf7c3
Tenzin Chophel <chopheltendi@gmail.com>
2025-05-30 12:02:31 +0600

    get to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "create_proof_request", to: "verifier#create_proof_request"
+  post "create_proof_request", to: "verifier#create_proof_request"
 
   resources :verifiable_credentials, only: [ :create ] do
     collection do
